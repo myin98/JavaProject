4f0ffeb8d026765132bf6855d56a3793a2c3c83d
myin98 <dnjsdydpf@gmail.com>
2024-06-07 14:24:37 +0900

    test

---
+++ WebServlet/src/com/servlet/Result.java
@@ -0,0 +1,27 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Result")
+public class Result extends HttpServlet {
+	
+	private final String page = "Result.jsp";
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Utils.print(request, response, page);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Utils.print(request, response, page);
+	}
+
+}
